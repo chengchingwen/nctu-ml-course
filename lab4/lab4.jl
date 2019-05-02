662fc79bc82db57882585e778a55c42b87ab286a
chengchingwen <adgjl5645@hotmail.com>
2019-05-02 22:46:09 +0800

    lab41 complete

--- lab4/lab4.jl
+++ lab4/lab4.jl
@@ -1,10 +1,10 @@
 using ArgParse
-#using Plots
+using Plots
 
 include("../lab3/rdg.jl")
-include("logi_regr.jl")
-include("../lab2/mnist.jl")
-include("em.jl")
+includet("logi_regr.jl")
+# include("../lab2/mnist.jl")
+# include("em.jl")
 
 function parse_cmd()
     s = ArgParseSettings()
@@ -55,9 +55,30 @@ end
 
 sample(dist::D, N) where D = hcat([sample(dist) for i = 1:N]...)
 
+function plot_result(m1, m2, x, y)
+    N = Int(length(y) / 2)
+    g = scatter(x[1,1:N], x[2,1:N], color=:blue, title="Ground truth")
+    scatter!(g, x[1,N+1:end], x[2,N+1:end], color=:red)
+
+    p1 = plot_result(string(m1.optm), m1, x)
+    p2 = plot_result(string(m2.optm), m2, x)
+
+    plot(g, p1, p2, layout=(1, 3), legend=false)
+end
+
+function plot_result(title, model, x)
+    p = classify(model, x)
+    b = map(first, filter(x->iszero(x[2]), collect(enumerate(p))))
+    r = map(first, filter(x->isone(x[2]), collect(enumerate(p))))
+    plt = scatter(x[1,b], x[2,b], color=:blue, title=title, legend=false)
+    scatter!(plt, x[1,r], x[2,r], color=:red, legend=false)
+
+    plt
+end
+
 function lab41(N, mx1, vx1, my1, vy1, mx2, vx2, my2, vy2)
-    D1dist = NvNormal([mx1, my1], [vx1 zero(vx1); zero(vy1) vy1])
-    D2dist = NvNormal([mx2, my2], [vx2 zero(vx2); zero(vy2) vy2])
+    D1dist = MvNormal([mx1, my1], [vx1 zero(vx1); zero(vy1) vy1])
+    D2dist = MvNormal([mx2, my2], [vx2 zero(vx2); zero(vy2) vy2])
     D1 = sample(D1dist, N) # label 0
     L1 = zeros(Float64, N)'
     D2 = sample(D2dist, N) # label 1
@@ -66,19 +87,15 @@ function lab41(N, mx1, vx1, my1, vy1, mx2, vx2, my2, vy2)
     L = hcat(L1, L2)
 
     model_sgd = LogisticRegression(2, SteepestGD())
-    model_nt = begin
-        m = copy(model_sgd)
-        m.optm = Newton()
-        m
-    end
+    model_nt = copy(model_sgd, Newton())
 
-    fit!(model_sgd, D, L) #TODO
+    fit!(model_sgd, D, L)
     fit!(model_nt,  D, L)
 
     display_summary(model_sgd, D, L)
     println("----------------------------------------")
     display_summary(model_nt , D, L)
-    #plot_result(model_sgd, model_nt, D, L)
+    plot_result(model_sgd, model_nt, D, L)
 end
 
 function lab42()
--- lab4/logi_regr.jl
+++ lab4/logi_regr.jl
@@ -2,53 +2,100 @@ using Printf
 
 abstract type OptMethod end
 
-struct Newton <: OptMethod end
 struct SteepestGD <: OptMethod end
+struct Newton <: OptMethod end
 
 Base.show(io::IO, optm::Newton) = print(io, "Newton's method")
 Base.show(io::IO, optm::SteepestGD) = print(io, "Gradient descent")
 
 σ(x) = one(x) / (one(x) + exp(-x))
 
-struct LogisticRegression
+mutable struct LogisticRegression
     Num::Int #number of feature
     θ::Array{Float64}
     optm::OptMethod
 end
 
+@inline bias(x) = vcat(ones(eltype(x), size(x, 2))', x)
+
 LogisticRegression(n::Int, optm = SteepestGD()) = LogisticRegression(n, randn(n+1)', optm)
 
-(lr::LogisticRegression)(x) = σ.(lr.θ * vcat(ones(eltype(x), size(x, 2))', x))
+(lr::LogisticRegression)(x) = σ.(lr.θ * bias(x))
 
-classify(lr::LogisticRegression, x) = round.(lr(x))
-
-function fit!(lr::LogisticRegression, x, y)
-    #TODO
-
-
-end
+classify(lr::LogisticRegression, x) = 1 .- round.(lr(x))
 
 function gradient(lr::LogisticRegression, x, y)
-    z = lr(x)
-    #TODO
+    p = lr(x)
+    x = bias(x)
+    - (1 .- y .- p) * x'
 end
 
 function hessian(lr::LogisticRegression, x, y)
-    z = lr(x)
-    #TODO
+    p = lr(x)
+    x = bias(x)
+    (x .* (p .* (1 .- p))) * x'
+end
+
+update_term(lr::LogisticRegression, x, y) = update_term(lr, x, y, lr.optm)
+
+function update_term(lr::LogisticRegression, x, y, nt::Newton)
+    H = hessian(lr, x, y)
+    if iszero(det(H))
+        update_term(lr, x, y, SteepestGD())
+    else
+        h = gradient(lr, x, y) * inv(H)'
+        - h ./ norm(h)
+    end
+end
+
+function update_term(lr::LogisticRegression, x, y, ::SteepestGD)
+    g = gradient(lr, x, y)
+    - g ./ norm(g)
+end
+
+function loglikelihood(θ, x, y)
+    z = θ * bias(x)
+    sum(log.(1 .+ exp.(-z)) .+ y .* z)
+end
+
+function fit!(lr::LogisticRegression, x, y; ϵ = 1e-6)
+    i = 0
+    θₙ = lr.θ
+    while (θₙ₊₁ = θₙ + update_term(lr, x, y); abs2(loglikelihood(θₙ₊₁, x, y) - loglikelihood(θₙ, x, y)) > ϵ && i < 100000)
+        θₙ .= θₙ₊₁
+        i += 1
+    end
+    lr
 end
 
 function confusion(lr::LogisticRegression, x, y)
     c = classify(lr, x)
     #compute confusion matrix cfm
-    #TODO
-    #end
+    TP = FP = TN = FN = 0
+    for (pred, label) ∈ zip(c, y)
+        if iszero(label)
+            if iszero(pred)
+                TP += 1
+            else
+                FN += 1
+            end
+        else
+            if iszero(pred)
+                FP += 1
+            else
+                TN += 1
+            end
+        end
+    end
+    cfm = [TP FN; FP TN]
     cfm
 end
 
-Base.copy(lr::LogisticRegression) = LogisticRegression(lr.Num, copy(lr.θ), lr.optm)
+sensitivity(cfm) = cfm[1, 1] / (cfm[1, 1] + cfm[1, 2])
+specificity(cfm) = cfm[2, 2] / (cfm[2, 2] + cfm[2, 1])
 
-
+Base.copy(lr::LogisticRegression) = copy(lr, lr.optm)
+Base.copy(lr::LogisticRegression, optm::OptMethod) = LogisticRegression(lr.Num, copy(lr.θ), optm)
 
 function display_summary(lr::LogisticRegression, x, y)
     cfm = confusion(lr, x, y)
@@ -63,8 +110,7 @@ function display_summary(lr::LogisticRegression, x, y)
     Printf.@printf "Is cluster 1 %9d %17d\n" cfm[1, 1] cfm[1, 2]
     Printf.@printf "Is cluster 2 %9d %17d\n" cfm[2, 1] cfm[2, 2]
     print("\n")
-    println("Sensitivity (Successfully predict cluster 1): $()") #TODO
-    println("Specificity (Successfully predict cluster 2): $()") #TODO
+    println("Sensitivity (Successfully predict cluster 1): $(sensitivity(cfm))")
+    println("Specificity (Successfully predict cluster 2): $(specificity(cfm))")
     print("\n")
 end
-
